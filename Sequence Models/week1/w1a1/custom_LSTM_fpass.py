c34b724310fd0cf89dba08ff81587b50d87e2983
rc-solon <84122060+rc-solon@users.noreply.github.com>
2023-08-11 16:57:45 +0200

    rename customRNN

--- Sequence Models/week1/w1a1/custom_LSTM_fpass.py
+++ Sequence Models/week1/w1a1/custom_LSTM_fpass.py
@@ -2,3 +2,15 @@
 In this section we are going to create the forward propagation of the LSTM
 """
 
+import numpy as np
+from rnn_utils import *
+from public_tests import *
+
+
+"""
+Definition of Gates: 
+
+
+
+
+"""
